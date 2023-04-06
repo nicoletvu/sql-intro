ac6e7381b23aad0b7269bc263a85131d54dfd0df
Nicole Vu <nicole.vu@kellogg.northwestern.edu>
2023-04-06 01:58:44 +0000

    COMMIT AGAIN

--- lab-4-3.sql
+++ lab-4-3.sql
@@ -6,4 +6,26 @@
 -- | New York Mets | Pete       | Alonso    | 53        |
 -- +---------------+------------+-----------+-----------+
 
-
+SELECT
+    team_id,
+    name,
+    player_id,
+    first_name,
+    last_name,
+    MAX(home_runs) AS most_home_runs
+FROM stats AS stats
+LEFT JOIN
+    (SELECT
+     id,
+     year,
+     name
+     FROM teams) AS teams
+     ON stats.team_id = teams.id
+LEFT JOIN
+    (SELECT
+     id,
+     first_name,
+     last_name
+     FROM players) AS players
+     ON stats.player_id = players.id
+WHERE year = 2019;
